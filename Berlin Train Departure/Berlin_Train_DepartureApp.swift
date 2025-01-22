b73c592cf64a19e64e3189a8db81487a8cfe2fd1
Justus Saringer <justus.saringer@check24.de>
2025-01-22 23:52:16 +0100

    Initial Commit

---
+++ Berlin Train Departure/Berlin_Train_DepartureApp.swift
@@ -0,0 +1,17 @@
+//
+//  Berlin_Train_DepartureApp.swift
+//  Berlin Train Departure
+//
+//  Created by Justus Saringer on 22.01.25.
+//
+
+import SwiftUI
+
+@main
+struct Berlin_Train_DepartureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Berlin Train Departure/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Berlin Train Departure
+//
+//  Created by Justus Saringer on 22.01.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
